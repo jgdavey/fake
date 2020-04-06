485660bc7527c755216da8120f18b80bd876d69c
Joshua Davey <josh@joshuadavey.com>
2020-04-06 10:45:44 -0400

    Use structopt for CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::env;
-use std::io::{self, BufRead, Error, ErrorKind};
 use std::io::prelude::*;
-use std::path::Path;
+use std::io::{self, BufRead, Error, ErrorKind};
+use std::path::PathBuf;
+
+use structopt::StructOpt;
 
 mod markov;
 
@@ -14,39 +15,60 @@ fn read_line(prompt: &str) -> io::Result<String> {
     line.unwrap_or_else(|| Err(Error::new(ErrorKind::Other, "EOF")))
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let default_file = String::from("example.txt");
-    let filename = match args.len() {
-        0..=1 => &default_file,
-        _ => &args[1],
-    };
+/// Generate text with markov chains
+#[derive(StructOpt, Debug)]
+#[structopt(name = "fake")]
+struct Config {
+    /// Activate debug mode
+    #[structopt(short, long)]
+    debug: bool,
 
+    /// File to process
+    #[structopt(name = "INPUT", parse(from_os_str))]
+    input: PathBuf,
+}
+
+fn setup_index(config: &Config) -> markov::Chain {
     let mut index = markov::Chain::new();
+    index.feed_file(&config.input).unwrap();
+    index
+}
 
-    index.feed_file(Path::new(filename)).unwrap();
-
-    index.printsizes();
-
+fn repl(mut index: markov::Chain) {
     loop {
         let res = read_line("seed> ");
         match res {
             Ok(ref seed) if seed == "" => {
                 println!("(Empty)");
-                if let Some(gen) = index.generate_best(140) {
+                if let Some(gen) = index.generate_best(100) {
                     println!("\n{}\n", gen);
                 }
-            },
+            }
             Ok(seed) => {
                 println!("{}", seed);
-                if let Some(gen) = index.generate_best_from(seed, 140) {
+                if let Some(gen) = index.generate_best_from(seed, 100) {
                     println!("\n{}\n", gen)
                 }
-            },
+            }
             Err(_) => {
                 println!("\nBye!");
-                break
+                break;
             }
         }
     }
+}
+
+fn main() {
+    let config = Config::from_args();
+    if config.debug {
+        println!("Config: {:?}", config);
+    }
+
+    let index = setup_index(&config);
+
+    if config.debug {
+        index.printsizes();
+    }
+
+    repl(index);
 }
